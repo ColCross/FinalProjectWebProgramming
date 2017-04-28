3cbd3699e9afef05a240d4cd7efcf0f028c51d1d
Colten Cross <colten@coltencross.com>
2017-04-27 20:54:04 -0400

    Styled profile page

--- profile_view.php
+++ profile_view.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -7,26 +7,38 @@
 <head>
     <meta charset="UTF-8">
     <title>Profile</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 
-<?php
+<div class="wrapper Absolute-Center">
+    <div class="content">
+        <?php
 
-// Check if the session has an id
-if (isset($_SESSION['id'])) {
+        // Check if the session has an id
+        if (isset($_SESSION['id'])) {
 
-    // Display user info
-    echo "<p>First name: " . $_SESSION["f_name"] . "</p>";
-    echo "<p>Last name: " . $_SESSION["l_name"] . "</p>";
-    echo "<p>User id: " . $_SESSION["id"] . "</p>";
+            // Display user info
+            echo "<div class='center-text'>";
+            echo "<p>First name: " . $_SESSION["f_name"] . "</p>";
+            echo "<p>Last name: " . $_SESSION["l_name"] . "</p>";
+            echo "<p>User id: " . $_SESSION["id"] . "</p>";
+            echo "</div>";
 
-} else {
+        } else {
 
-    // They are not logged in
-    echo "<p>You are not logged in.</p>";
-    echo "<a href='./login_view.php'>Login</a><a href='./register_view.php'>Register</a>";
-}
-?>
-
+            // They are not logged in
+            echo "<div class='center-text'>";
+            echo "<h2>You are not logged in</h2>";
+            echo "<br>";
+            echo "<a href='./login_view.php' style='padding-right: 20px;'>Login</a><a href='./register_view.php'>Register</a>";
+            echo "</div>";
+        }
+        ?>
+    </div>
+</div>
 </body>
 </html>
