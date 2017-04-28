48f82ee1caedfbbc171b6a7cb8e47b70dbd04d5e
Colten Cross <colten@coltencross.com>
2017-04-27 21:50:32 -0400

    Fixed checkout_view styling

--- checkout_view.php
+++ checkout_view.php
@@ -14,36 +14,39 @@ session_start();
 </head>
 <body>
 
-<?php
+<div class="wrapper">
+    <?php
 
-$total = 0;
+    $total = 0;
 
-// Check for a cart in the session
-if (isset($_SESSION["cart"]) && isset($_SESSION["inventory"])) {
+    // Check for a cart in the session
+    if (isset($_SESSION["cart"]) && isset($_SESSION["inventory"])) {
 
-    // Sum everything in the cart
-    foreach ($_SESSION["cart"] as $item) {
-        foreach ($_SESSION["inventory"] as $row) {
-            if($item[0] == $row["id"]){
-                $total = $total + ($row["price"] * $item[1]);
+        // Sum everything in the cart
+        foreach ($_SESSION["cart"] as $item) {
+            foreach ($_SESSION["inventory"] as $row) {
+                if ($item[0] == $row["id"]) {
+                    $total = $total + ($row["price"] * $item[1]);
+                }
             }
         }
+
+        // Display the total
+        echo "<p>Total: $" . $total . "</p>";
+
+        // Remove the inventory from the session
+        unset($_SESSION["inventory"]);
+
+    } else {
+
+        // Send to the store front
+        header("Location: ./checkout_control.php");
+        exit;
     }
 
-    // Display the total
-    echo "<p>Total: $" . $total ."</p>";
+    ?>
 
-    // Remove the inventory from the session
-    unset($_SESSION["inventory"]);
-
-} else {
-
-    // Send to the store front
-    header("Location: ./checkout_control.php");
-    exit;
-}
-
-?>
+</div>
 
 </body>
 </html>
